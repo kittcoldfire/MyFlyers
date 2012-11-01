f69221151025bc6ec53adcd5b8c9cd21e187ac24
Phil Tucker <kittcoldfire@gmail.com>
2012-11-01 16:34:11 -0400

    Initial commit - myflyers

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyflyersApp::Application.config.secret_token = 'ceeda080d530ad96857cc7337229e9f8c963c755e90383a928f2a9b840a06734fe3ae260387bad54775e5f054125979049a440c81f040cfc419608bd8213cbaa'
